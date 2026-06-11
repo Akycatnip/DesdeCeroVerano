a2aa6da2cd4c64e49e49931fbb434f0dc9a2c292
Marta Moreno Aranda <you@example.com>
2026-06-11 18:44:54 +0200

    listo por hoy

--- Dia1.py
+++ Dia1.py
@@ -23,10 +23,68 @@
 # else:
 #     print("Es negativo")
 
-#Pide la edad de una persona e indica si es mayor de edad.
+# #Pide la edad de una persona e indica si es mayor de edad.
+#
+# edad = int(input("¿Cuántos años tienes?\n"))
+# if edad >= 18:
+#     print("Eres mayor de edad")
+# else:
+#     print("Fuera de aquí enano!")
+#
+# #Pide dos números e indica cuál es mayor.
+#
+# numero1= int(input("Dime el primer numero"))
+# numero2= int(input("Dime el segundo numero"))
+# if numero1 > numero2:
+#     print(f"{numero1} es mayor que {numero2}")
+# elif numero1==numero2:
+#     print(f"{numero2} es igual a {numero1}")
+# else:
+#     print(f"{numero2} es mayor que {numero1}")
 
-edad = int(input("¿Cuántos años tienes?\n"))
-if edad >= 18:
-    print("Eres mayor de edad")
-else:
-    print("Fuera de aquí enano!")
+#Muestra los números del 1 al 10 usando un bucle.
+#
+# numero = 1
+# for numero in range(0,10):
+#     print(numero+1)
+
+#Muestra los números pares entre 1 y 20.
+#
+# for numero in range(1,21):
+#     if(numero%2==0):
+#         print(numero)
+#
+# #Pide un número y muestra su tabla de multiplicar.
+#
+# numero = int(input("Dame un numero:\n "))
+# for i in range(0,11):
+#     print(numero*i)
+#
+# #Calcula la suma de los números del 1 al 100.
+# suma =0
+# for i in range(0,101):
+#     suma +=i
+#     print(suma)
+
+# #Cuenta cuántos números pares hay entre 1 y 100.
+# contador =0
+# for i in range(1,101):
+#     if i%2==0:
+#         contador +=1
+# print(contador)
+
+#Pide 5 números al usuario y muestra cuál es el mayor.
+
+numero1 = int(input("Dame el primer numero: "))
+numero2 = int(input("Dame el segundo numero: "))
+numero3 = int(input("Dame el tercer numero: "))
+numero4 = int(input("Dame el cuarto numero: "))
+numero5 = int(input("Dame el quinto numero: "))
+
+lista = [numero1, numero2, numero3, numero4, numero5]
+
+
+
+
+
+#13
