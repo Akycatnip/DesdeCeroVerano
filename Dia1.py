1cf47f3201aa0f98cabba02275ec6bd2a170aa9f
Marta Moreno Aranda <you@example.com>
2026-06-11 17:43:06 +0200

    Initial commit

---
+++ Dia1.py
@@ -0,0 +1,32 @@
+# #Crea una variable con tu nombre y otra con tu edad. Muestra ambas por pantalla.
+# nombre = "Marta"
+# edad = 42
+# print("Mi nombre es:",nombre, "y tengo", edad ,"años")
+#
+# #Pide al usuario su nombre mediante input() y salúdalo
+#
+# tuNombre= input("Cómo te llamas?\n")
+# print("Hola",tuNombre,"!")
+#
+# #Pide dos números y muestra su suma.
+#
+# numero1=int (input("Dame el primer número.\n"))
+# numero2=int (input("Dame el segundo número.\n"))
+# suma = print("La suma de los dos es: ",(numero1+numero2))
+
+# #Pide un número e indica si es positivo, negativo o cero.
+# numero = int(input("Dame un número.\n"))
+# if numero > 0:
+#     print("Es positivo")
+# elif numero == 0:
+#     print("Es cero")
+# else:
+#     print("Es negativo")
+
+#Pide la edad de una persona e indica si es mayor de edad.
+
+edad = int(input("¿Cuántos años tienes?\n"))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Fuera de aquí enano!")
